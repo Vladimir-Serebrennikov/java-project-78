fda30ff2c5c7f45602f67bf8d8a865b170fa1770
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-10-27 21:45:15 +0300

    refactoring

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -10,82 +10,82 @@ import static org.junit.jupiter.api.Assertions.assertTrue;;
 public class ValidatorTest {
     @Test
     public void testRequiredWithNonNullString() {
-        StringSchema schema = new StringSchema();
+        var schema = new StringSchema();
         assertTrue(schema.required().isValid("TestString"));
     }
     @Test
     public void testRequiredWithNullString() {
-        StringSchema schema = new StringSchema();
+        var schema = new StringSchema();
         assertFalse(schema.required().isValid(null));
     }
     @Test
     public void testMinLengthWithValidString() {
-        StringSchema schema = new StringSchema();
+        var schema = new StringSchema();
         assertTrue(schema.minLength(5).isValid("ValidString"));
     }
     @Test
     public void testMinLengthWithInvalidString() {
-        StringSchema schema = new StringSchema();
+        var schema = new StringSchema();
         assertFalse(schema.minLength(10).isValid("Short"));
     }
     @Test
     public void testContainsWithContainedSubstring() {
-        StringSchema schema = new StringSchema();
+        var schema = new StringSchema();
         assertTrue(schema.contains("apple").isValid("I love apples"));
     }
     @Test
     public void testContainsWithNonContainedSubstring() {
-        StringSchema schema = new StringSchema();
+        var schema = new StringSchema();
         assertFalse(schema.contains("fruits").isValid("I love apples"));
     }
     @Test
     public void testRequiredWithValidValue() {
-        NumberSchema schema = new NumberSchema();
+        var schema = new NumberSchema();
         Object validNumber = 77;
         assertTrue(schema.required().isValid(validNumber));
     }
     @Test
     public void testRequiredWithNullValue() {
-        NumberSchema schema = new NumberSchema();
+        var schema = new NumberSchema();
         assertFalse(schema.required().isValid(null));
     }
     @Test
     public void testRequiredWithNonNumberValue() {
-        NumberSchema schema = new NumberSchema();
+        var schema = new NumberSchema();
         Object nonNumberValue = "String";
         assertFalse(schema.required().isValid(nonNumberValue));
     }
     @Test
     public void testPositiveWithPositiveValue() {
-        NumberSchema schema = new NumberSchema();
+        var schema = new NumberSchema();
         Object positiveNumber = 10;
 
         assertTrue(schema.positive().isValid(positiveNumber));
     }
     @Test
     public void testPositiveWithNegativeValue() {
-        NumberSchema schema = new NumberSchema();
+        var schema = new NumberSchema();
         Object negativeNumber = -5;
 
         assertFalse(schema.positive().isValid(negativeNumber));
     }
     @Test
     public void testRangeWithInclusiveValue() {
-        NumberSchema schema = new NumberSchema();
+        var schema = new NumberSchema();
         Object valueInRange = 5;
 
         assertTrue(schema.range(5, 10).isValid(valueInRange));
     }
     @Test
     public void testRangeWithOutOfRangeValue() {
-        NumberSchema schema = new NumberSchema();
+        var schema = new NumberSchema();
         Object valueOutOfRange = 3;
 
         assertFalse(schema.range(5, 10).isValid(valueOutOfRange));
     }
     @Test
     public void testRequiredWithValidMap() {
-        MapSchema schema = new MapSchema();
+        var schema = new MapSchema();
         Map<String, Integer> validMap = new HashMap<>();
         validMap.put("key1", 1);
         validMap.put("key2", 2);
@@ -93,13 +93,13 @@ public class ValidatorTest {
     }
     @Test
     public void testRequiredWithNullMap() {
-        MapSchema schema = new MapSchema();
+        var schema = new MapSchema();
         assertFalse(schema.required().isValid(null));
     }
 
     @Test
     public void testSizeOfWithValidSize() {
-        MapSchema schema = new MapSchema();
+        var schema = new MapSchema();
         Map<String, Integer> validMap = new HashMap<>();
         validMap.put("key1", 1);
         validMap.put("key2", 2);
@@ -109,7 +109,7 @@ public class ValidatorTest {
 
     @Test
     public void testSizeOfWithInvalidSize() {
-        MapSchema schema = new MapSchema();
+        var schema = new MapSchema();
         Map<String, Integer> validMap = new HashMap<>();
         validMap.put("key1", 1);
         validMap.put("key2", 2);
@@ -118,7 +118,7 @@ public class ValidatorTest {
     }
     @Test
     public void testShapeWithValidMap() {
-        MapSchema schema = new MapSchema();
+        var schema = new MapSchema();
         Map<String, BaseSchema> schemas = new HashMap<>();
         schemas.put("key1", new NumberSchema().positive());
         schemas.put("key2", new StringSchema().minLength(3));
@@ -130,7 +130,7 @@ public class ValidatorTest {
     }
     @Test
     public void testShapeWithInvalidMap() {
-        MapSchema schema = new MapSchema();
+        var schema = new MapSchema();
         Map<String, BaseSchema> schemas = new HashMap<>();
         schemas.put("key1", new NumberSchema().positive());
         schemas.put("key2", new StringSchema().minLength(3));
