e116eebba95bca0f15e5fa0e74c30eb01101056c
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-10-28 14:16:50 +0300

    adding access final

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -10,15 +10,11 @@ public abstract class BaseSchema {
         this.checks = new HashMap<>();
     }
 
-/*
- * Adds a new validation rule to the schema. This method is intended to be overridden
- * in subclasses to add custom validation rules.
- */
-    public void addCheck(String nameSchema, Predicate<Object> check) {
+    public final void addCheck(String nameSchema, Predicate<Object> check) {
         checks.put(nameSchema, check);
     }
 
-    public boolean isValid(Object obj) {
+    public final boolean isValid(Object obj) {
         return checks.values()
                 .stream()
                 .allMatch(check -> check.test(obj));
