088bad3eb262c9723aae937502786e116ca375d8
Vladimir Serebrennikov <serebvl23@gmail.com>
2023-10-28 13:00:06 +0300

    update NumberSchema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -3,6 +3,7 @@ package hexlet.code.schemas;
 import java.util.Objects;
 
 public class NumberSchema extends BaseSchema {
+
     public NumberSchema required() {
         addCheck("required",
                 value -> Objects.nonNull(value) && value instanceof Number);
@@ -11,13 +12,14 @@ public class NumberSchema extends BaseSchema {
 
     public NumberSchema positive() {
         addCheck("positive",
-                value -> value == null || ((Integer) value) > 0);
+                value -> value instanceof Number && (value == null || ((Integer) value) > 0));
         return this;
     }
 
     public NumberSchema range(int start, int end) {
         addCheck("range",
-                value -> value == null || (Integer) value >= start && (Integer) value <= end);
+                value -> value instanceof Number && (value == null || (Integer) value >= start
+                        && (Integer) value <= end));
         return this;
     }
 
